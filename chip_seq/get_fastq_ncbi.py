502bc29ad9813d08be301ecc6cdedb947583525a
Michael Freitas <mfreitas@Michaels-iMac-Pro.local>
2019-01-26 10:06:31 -0500

    updated download protocols

--- chip_seq/get_fastq_ncbi.py
+++ chip_seq/get_fastq_ncbi.py
@@ -14,11 +14,22 @@ with open(sys.argv[1]) as f:
             header = lis
             first_line = False
             print("Header Found")
-            fastq_index = header.index("run_accession")
-            print("fastq_ftp index located in column {}".format(fastq_index))
+            run_accession = header.index("run_accession")
+            sra_ftp = header.index("sra_ftp")
+            print("fastq_ftp index located in column {}".format(sra_ftp))
 
         else:
-            print(lis[fastq_index])
-            subprocess.run(["fastq-dump", "-v","--readids",
-                "--split-3", lis[fastq_index]])
-            #subprocess.run(["fastq-dump", lis[fastq_index]])
+            sra = lis[run_accession]
+
+            loc = "/sra/sra-instant/reads/ByRun/sra/{}/{}/{}/{}.sra".format(sra[0:3],sra[0:6],sra,sra)
+            wget_loc = "ftp://ftp-trace.ncbi.nih.gov" + loc
+            ascp_loc = "anonftp@ftp.ncbi.nlm.nih.gov:" + loc
+            ascp_rsa = "/root/.aspera/connect/etc/asperaweb_id_dsa.openssh"
+
+            #subprocess.run(["wget", "--continue", "--progress=bar", wget_loc])
+            ascp_cmd = ["ascp", "-T", "-k", "1", "-l","100m","-i",ascp_rsa,ascp_loc,"."]
+            fastq_dump_cmd = ["fastq-dump","--gzip","-I","--split-files","./"+sra+".sra"]
+            print("downloading sra file {}".format(sra))
+            subprocess.run(ascp_cmd)
+            print("Converting sra file {} to fastq.gz".format(sra))
+            subprocess.run(fastq_dump_cmd)
